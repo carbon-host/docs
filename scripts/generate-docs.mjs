f9170bda824bf18fe9b5c29752a916b46ab83c84
Dawson <hello@dawson.gg>
2025-01-24 17:15:59 -0500

    Update meta, clean up

--- scripts/generate-docs.mjs
+++ scripts/generate-docs.mjs
@@ -1,18 +1,18 @@
 import * as OpenAPI from 'fumadocs-openapi';
 import { rimrafSync } from 'rimraf';
 
-const out = './content/docs/openapi';
+const out = '/content/docs/openapi';
 
 // clean generated files
-rimrafSync(out, {
-  filter(v) {
-    return !v.endsWith('index.mdx') && !v.endsWith('meta.json');
-  },
-});
+// rimrafSync(out, {
+//   filter(v) {
+//     return !v.endsWith('index.mdx') && !v.endsWith('meta.json');
+//   },
+// });
 
 void OpenAPI.generateFiles({
   // input files
   input: ['./openapi.json'],
-  output: `/content/docs`,
+  output: `/content/docs/openapi`,
   groupBy: 'tag',
 });
